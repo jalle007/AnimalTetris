9264ffb44e33fa592787e8b62a10a43fb7241baf
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-09-05 22:09:18 +0200

    1st

---
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+//		initialize(new MyGdxGame(), config);
+		initialize(new Physics2() {
+			@Override
+			public void resize(int width, int height) {
+
+			}
+
+			@Override
+			public void pause() {
+
+			}
+
+			@Override
+			public void resume() {
+
+			}
+
+			@Override
+			public void dispose() {
+
+			}
+		}, config);
+	}
+}
